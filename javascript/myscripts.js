b50380cbb3ac821b80873938b42046fef642e898
Paruyr23 <par23mar5@gmail.com>
2020-01-23 03:37:40 +0400

    Added changes in this file

--- javascript/myscripts.js
+++ javascript/myscripts.js
@@ -1,26 +1,28 @@
-$(function() {
-
-    $(".progress").each(function() {
-
-        var value = $(this).attr('data-value');
-        var left = $(this).find('.progress-left .progress-bar');
-        var right = $(this).find('.progress-right .progress-bar');
-
-        if (value > 0) {
-            if (value <= 50) {
-                right.css('transform', 'rotate(' + percentageToDegrees(value) + 'deg)')
-            } else {
-                right.css('transform', 'rotate(180deg)')
-                left.css('transform', 'rotate(' + percentageToDegrees(value - 43) + 'deg)')
+$(document).ready(function ($) {
+    function animateElements() {
+        $('.progressbar').each(function () {
+            var elementPos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            var percent = $(this).find('.circle').attr('data-percent');
+            var percentage = parseInt(percent, 10) / parseInt(100,10);
+            var animate = $(this).data('animate');
+            if (elementPos < topOfWindow + $(window).height() - 30 && !animate) {
+                $(this).data('animate', true);
+                $(this).find('.circle').circleProgress({
+                    startAngle: -Math.PI / -11,
+                    value: percent / 100,
+                    size: 180,
+                    thickness: 5,
+                    emptyFill: "rgba(0,0,0, .2)",
+                    fill: {
+                        color: '#ff0036'
+                    }
+                }).on('circle-animation-progress', function (event, progress, stepValue) {
+                    $(this).find('div').text((stepValue*100).toFixed(0) + "%");
+                }).stop();
             }
-        }
-
-    })
-
-    function percentageToDegrees(percentage) {
-
-        return percentage / 100 * 360
-
+        });
     }
-
+    animateElements();
+    $(window).scroll(animateElements);
 });
